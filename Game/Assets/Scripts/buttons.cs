07be09bf8a5e9cafc20830b82b7f8ca8d55a7e82
Ali Babaei <alibabaei12@gmail.com>
2019-03-09 01:51:37 -0800

    main menu completed

--- Game/Assets/Scripts/buttons.cs
+++ Game/Assets/Scripts/buttons.cs
@@ -1,13 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement; 
 
 public class buttons : MonoBehaviour
 {
     public void play_game()
     {
 
-        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+        SceneManager.LoadScene(1);
     }
+    public void back()
+    {
+
+        SceneManager.LoadScene(0);
+    }
+    public void credit()
+    {
+
+        SceneManager.LoadScene(2);
+    }
+    public void options()
+    {
+
+        SceneManager.LoadScene(3);
+    }
+
 }
