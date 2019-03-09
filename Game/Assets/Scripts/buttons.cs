f86d0760dcddecc1f5fe33b0325c2e4bebc1cb09
Ali Babaei <alibabaei12@gmail.com>
2019-03-09 01:38:02 -0800

    main meneu play game button

---
+++ Game/Assets/Scripts/buttons.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class buttons : MonoBehaviour
+{
+    public void play_game()
+    {
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+}
