4d3e390846f25e062fba9ef6827ae670903ca397
p-d-c <34993622+p-d-c@users.noreply.github.com>
2019-03-21 19:19:17 -0700

    Added comments to inventory

--- Game/Assets/StackableInventory/Assets/Scripts/Inventory.cs
+++ Game/Assets/StackableInventory/Assets/Scripts/Inventory.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Module creates menu in Unity inspector for Inventory gameobject
 [CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory/New Inventory")]
 public class Inventory : ItemHolder
 {
