cb44d2762ae037799ea2076b7892be0e88031544
p-d-c <34993622+p-d-c@users.noreply.github.com>
2019-03-21 19:17:00 -0700

    Added comments to inventory

--- Game/Assets/StackableInventory/Assets/Scripts/Interactable.cs
+++ Game/Assets/StackableInventory/Assets/Scripts/Interactable.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Module attached to items to make them collectable
 public class Interactable : MonoBehaviour
 {
     public virtual void Interact()
