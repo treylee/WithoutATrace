298bab96e437b25d7c55926dd5bd61c41b44ed94
trey <trlcoope@ucsc.edu>
2019-05-23 10:38:55 -0700

    Sorting Scene

---
+++ Game/Assets/SortingSceneScripts/ItemHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<GameObject> items = new List<GameObject>();
+    public int curItem = 0;
+    public bool changeItem = false;
+    void Start()
+    {
+        foreach (Transform x in transform)
+        {
+            items.Add(x.gameObject);
+        }
+       
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (curItem <= items.Count - 1)
+        {
+            items[curItem].SetActive(true);
+        }
+    }
+}
---
+++ Game/Assets/SortingSceneScripts/NodeHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeHandler : MonoBehaviour //compares the current Item to the node
+{
+    // Start is called before the first frame update
+    string currentItemName ="";
+    public GameObject itemScript;
+    ItemHandler itemHandler;
+    string x;
+    void Start()
+    {
+
+        itemHandler = itemScript.GetComponent<ItemHandler>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    //    Debug.Log("items =" + itemHandler.items.Count);
+
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            if (itemHandler.items.Count > 0)
+            {
+                currentItemName = itemHandler.items[0].tag;
+                Debug.Log("name =" + currentItemName);
+            }
+
+
+        }
+        Debug.Log("entered trugeer");
+
+    }
+  
+}
